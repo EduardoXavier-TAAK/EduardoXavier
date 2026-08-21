f420e6edeef0c097f8f86ae3bee4472da7fc1488
EduardoXavier-TAAK <eduardo.xavier@taak.com.br>
2026-08-21 14:38:13 -0300

    feat: Caelum erros e exceções

---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class main {
+    
+	List<Integer> ints = new List<Integer>();
+
+
+    ints.add(num1);
+    ints.add(num2); 
+    ints.add(num3);
+
+    Integer ref = 0;
+    for(Integer i = 0; i < 3; i++){
+        if(ints[i] > ref){
+            ref = ints[i]
+        }
+    }
+
+    return ref;
+    }
+
